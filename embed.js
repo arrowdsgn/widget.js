98fb1bd1ecc9b30debb8603d6913a63422faed95
arrowdsgn <d.arrowdsgn@gmail.com>
2024-12-23 19:58:24 -0400

    Create embed.js

---
+++ embed.js
@@ -0,0 +1,7 @@
+// Embed WhatsApp Widget
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://tu-usuario.github.io/whatsapp-widget/widget.js"; // Reemplaza con tu enlace real
+  script.async = true;
+  document.body.appendChild(script);
+})();
